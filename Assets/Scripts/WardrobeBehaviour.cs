436fe437a0b5e8f8c8b4689fd8bed1308687f6a1
Elizaveta Zalomnova <61969421+handlessdude@users.noreply.github.com>
2024-12-16 04:05:08 +0300

    add SwitchAccessory

--- Assets/Scripts/WardrobeBehaviour.cs
+++ Assets/Scripts/WardrobeBehaviour.cs
@@ -43,6 +43,20 @@ public class WardrobeBehaviour : MonoBehaviour
     public void SetNextHeadAccessory()
     {
         SwitchHeadAccessory(true);
+        
+        /*
+         *SwitchAccessory(
+        ref currentHeadAccessory,
+        headAccessorySlot,
+        accessoriesData.HeadAccessories,
+        updatedAccessory => currentHeadAccessory = updatedAccessory,
+        isNext
+    );
+
+    OnHatAccessoryChange?.Invoke(getAccessoryType());
+         * 
+         */
+        
     }    
     
     public void SetPreviousHeadAccessory()
@@ -106,6 +120,65 @@ public class WardrobeBehaviour : MonoBehaviour
         OnHatAccessoryChange?.Invoke(getAccessoryType());
     }
 
+    private void SwitchAccessory(
+        ref GameObject currentAccessory,
+        GameObject accessorySlot,
+        List<GameObject> accessories,
+        Action<GameObject> updateCurrentAccessory,
+        bool isNext = true)
+    {
+        if (accessories == null || accessories.Count == 0)
+        {
+            Debug.LogError("Accessories list is empty.");
+            return;
+        }
+
+        int threshold = isNext ? accessories.Count : -1;
+        int step = isNext ? 1 : -1;
+        int defaultAccessoryIndex = (accessories.Count + threshold) % accessories.Count;
+
+        // Remove the current accessory
+        if (currentAccessory != null)
+        {
+            Destroy(currentAccessory);
+
+            // Find the current accessory's index in the list
+            int currentIndex = accessories.FindIndex(
+                accessory => currentAccessory.name.StartsWith(accessory.name)
+            );
+
+            if (currentIndex == -1)
+            {
+                Debug.LogError("Accessory not found in list!");
+                return;
+            }
+
+            int nextIndex = currentIndex + step;
+
+            if (nextIndex == threshold)
+            {
+                currentAccessory = null;
+            }
+            else
+            {
+                currentAccessory = Instantiate(accessories[nextIndex], accessorySlot.transform);
+            }
+        }
+        else
+        {
+            currentAccessory = Instantiate(accessories[defaultAccessoryIndex], accessorySlot.transform);
+        }
+
+        if (currentAccessory != null)
+        {
+            currentAccessory.transform.localPosition = Vector3.zero;
+            currentAccessory.transform.localRotation = Quaternion.identity;
+        }
+
+        // Update the current accessory reference
+        updateCurrentAccessory(currentAccessory);
+    }
+    
     private string getAccessoryType()
     {
         if (currentHeadAccessory == null)

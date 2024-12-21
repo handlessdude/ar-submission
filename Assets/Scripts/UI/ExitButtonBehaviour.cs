4812a2b7ba9a4bf411a08a3d361813422423f297
Elizaveta Zalomnova <61969421+handlessdude@users.noreply.github.com>
2024-12-21 05:26:10 +0300

    add exit button

---
+++ Assets/Scripts/UI/ExitButtonBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class ExitButtonBehaviour : MonoBehaviour
+{
+    private GameObject LoadingCaption;
+    private GameObject UserMenu;
+    
+    private GameObject DraggingCaption;
+    
+    private void Start()
+    {
+        //  trackedImageModel = GameObject.FindGameObjectWithTag("TrackedImageModel");
+
+        LoadingCaption = GameObject.FindGameObjectWithTag("LoadingCaption");
+        if (LoadingCaption == null)
+        {
+            MyLogger.Log("LoadingCaption not found");
+        }
+        UserMenu = GameObject.FindGameObjectWithTag("UserMenu");
+        if (UserMenu == null)
+        {
+            MyLogger.Log("UserMenu not found");
+        }
+        
+        DraggingCaption = GameObject.FindGameObjectWithTag("DraggingCaption");
+        if (DraggingCaption == null)
+        {
+            MyLogger.Log("DraggingCaption not found");
+        }
+        
+        var button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            ToggleLoadingCaption();
+            SceneManager.LoadScene("MainMenuScene");
+        });
+    }
+    
+    private void ToggleLoadingCaption()
+    {
+        if (LoadingCaption != null && UserMenu != null)
+        {
+            DraggingCaption.SetActive(false);
+            LoadingCaption.SetActive(true);
+            UserMenu.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ScreenshotButtonBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ScreenshotButtonBehaviour : MonoBehaviour
+{
+    public string screenshotFileName = "Screenshot";
+
+    public void TakeScreenshot()
+    {
+        // Generate a unique filename with a timestamp
+        string filePath = $"{Application.persistentDataPath}/{screenshotFileName}_{System.DateTime.Now:yyyyMMdd_HHmmss}.png";
+        
+        // Capture the screenshot and save it to the specified path
+        ScreenCapture.CaptureScreenshot(filePath);
+        
+        Debug.Log($"Screenshot saved to: {filePath}");
+    }
+}
